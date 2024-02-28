e1a26f7e15c7ec76e15e7ff9e7ac6936b81d948b
Justin <justin@onepage.com.hk>
2024-02-28 11:23:02 +0000

    Work on feed Subscribe

---
+++ adafruitio_mqtt_feed_recevied/adafruitio_mqtt_feed_recevied.ino
@@ -0,0 +1,205 @@
+/***********************************************************************
+  Adafruit MQTT Library ESP32 Adafruit IO SSL/TLS example
+
+  Use the latest version of the ESP32 Arduino Core:
+    https://github.com/espressif/arduino-esp32
+
+  Works great with Adafruit Huzzah32 Feather and Breakout Board:
+    https://www.adafruit.com/product/3405
+    https://www.adafruit.com/products/4172
+
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
+
+  Written by Tony DiCola for Adafruit Industries.
+  Modified by Brent Rubell for Adafruit Industries
+  MIT license, all text above must be included in any redistribution
+ **********************************************************************/
+#include <WiFi.h>
+#include "WiFiClientSecure.h"
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+#include <Adafruit_Sensor.h>
+// #include <DHT.h>
+// #include <DHT_U.h>
+#include "config.h"
+
+/************ Global State (you don't need to change this!) ******************/
+
+// WiFiFlientSecure for SSL/TLS support
+WiFiClientSecure client;
+
+// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+
+/****************************** Feeds ***************************************/
+
+// Setup a feed called 'test' for publishing.
+// Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname>
+Adafruit_MQTT_Publish ldr = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/ldr");
+Adafruit_MQTT_Subscribe onoffbutton = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/onoffbutton");
+// Adafruit_MQTT_Publish temperature = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/temperature");
+// Adafruit_MQTT_Publish humidity = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/humidity");
+
+// Adafruit_MQTT_Subscribe ONOFF_FEED = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/onoffbutton");
+// Adafruit_MQTT_Subscribe onoffbutton = Adafruit_MQTT_Subscribe(&mqtt, ONOFF_FEED);
+
+#define LED_PIN 27
+#define LDR_PIN 32
+
+int ldrValue = 0;
+
+// DHT_Unified dht(DATA_PIN, DHT22);
+
+/*************************** Sketch Code ************************************/
+
+void setup()
+{
+  Serial.begin(115200);
+  delay(10);
+
+  Serial.println(F("Adafruit IO MQTTS (SSL/TLS) Example"));
+
+  // Connect to WiFi access point.
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WLAN_SSID);
+
+  // dht.begin();
+
+  delay(1000);
+
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
+  delay(2000);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  pinMode(LED_PIN, OUTPUT);
+
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Set Adafruit IO's root CA
+  client.setCACert(adafruitio_root_ca);
+}
+
+// uint32_t x = 0;
+
+void loop()
+{
+  // Ensure the connection to the MQTT server is alive (this will make the first
+  // connection and automatically reconnect when disconnected).  See the MQTT_connect
+  // function definition further below.
+  MQTT_connect();
+
+  // sensors_event_t event;
+  // dht.temperature().getEvent(&event);
+
+  // float celsius = event.temperature;
+  // float fahrenheit = (celsius * 1.8) + 32;
+  //
+  // Serial.print("celsius: ");
+  // Serial.print(celsius);
+  // Serial.println("C");
+  //
+  // Serial.print("fahrenheit: ");
+  // Serial.print(fahrenheit);
+  // Serial.println("F");
+  //
+  // // Now we can publish stuff!
+  // Serial.print(F("\nSending val "));
+  // Serial.print(x);
+  // Serial.print(F(" to test feed..."));
+  //
+  // dht.humidity().getEvent(&event);
+  //
+  // Serial.print("humidity: ");
+  // Serial.print(event.relative_humidity);
+  // Serial.println("%");
+  //
+  // temperature.publish(fahrenheit);
+  // humidity.publish(event.relative_humidity);
+
+  ldrValue = analogRead(LDR_PIN);
+  Serial.print("LDR Value: ");
+  Serial.println(ldrValue);
+
+  mqtt.subscribe(&onoffbutton);
+
+  Adafruit_MQTT_Subscribe *subscription;
+  while ((subscription = mqtt.readSubscription(5000)))
+  {
+    // Check if its the onoff button feed
+    if (subscription == &onoffbutton)
+    {
+      Serial.print(F("On-Off button: "));
+      Serial.println((char *)onoffbutton.lastread);
+
+      if (strcmp((char *)onoffbutton.lastread, "ON") == 0)
+      {
+
+        digitalWrite(LED_PIN, LOW);
+
+        ldrValue = analogRead(LDR_PIN);
+        ldr.publish(ldrValue);
+      }
+      if (strcmp((char *)onoffbutton.lastread, "OFF") == 0)
+      {
+        digitalWrite(LED_PIN, HIGH);
+      }
+    }
+  }
+
+  // if (!test.publish(x++))
+  // {
+  //   Serial.println(F("Failed"));
+  // }
+  // else
+  // {
+  //   Serial.println(F("OK!"));
+  // }
+
+  // wait a couple seconds to avoid rate limit
+  // delay(5000);
+}
+
+// Function to connect and reconnect as necessary to the MQTT server.
+// Should be called in the loop function and it will take care if connecting.
+void MQTT_connect()
+{
+  int8_t ret;
+
+  // Stop if already connected.
+  if (mqtt.connected())
+  {
+    return;
+  }
+
+  Serial.print("Connecting to MQTT... ");
+
+  uint8_t retries = 3;
+  while ((ret = mqtt.connect()) != 0)
+  { // connect will return 0 for connected
+    Serial.println(mqtt.connectErrorString(ret));
+    Serial.println("Retrying MQTT connection in 5 seconds...");
+    mqtt.disconnect();
+    delay(5000); // wait 5 seconds
+    retries--;
+    if (retries == 0)
+    {
+      // basically die and wait for WDT to reset me
+      while (1)
+        ;
+    }
+  }
+
+  Serial.println("MQTT Connected!");
+}
